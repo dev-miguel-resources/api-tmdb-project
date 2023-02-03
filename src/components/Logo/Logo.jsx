5ca4457a4ce3b0b642a947844b4cff61b05ecd5d
dev-miguel-resources <ingenieromiguelch@gmail.com>
2023-02-03 13:38:51 -0300

    explain styled components in Logo Component

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,3 +1,4 @@
+import { LogoContainer, LogoImg, LogoTitle } from "./StyledLogo";
 import s from "./style.module.css";
 
 // STATELESS / SIN ESTADO / SIN COMPORTAMIENTO PROPIO
@@ -9,5 +10,15 @@ const Logo = ({ title, image }) => (
     </div>
   </div>
 );
+
+// STYLED-COMPONENTS
+/*const Logo = ({ title, image }) => (
+  <div>
+    <LogoContainer>
+      <LogoImg src={image} alt="logo" />
+      <LogoTitle>{title}</LogoTitle>
+    </LogoContainer>
+  </div>
+);*/
 
 export default Logo;
